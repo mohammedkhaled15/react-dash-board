c7fd97dd63fb8367de44bc37f0842187a64c7209
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-10-02 13:57:05 +0200

    ..

--- src/components/sharedLayout/SideNavbar.jsx
+++ src/components/sharedLayout/SideNavbar.jsx
@@ -47,17 +47,17 @@ const linksData = [
 
 const SideNavbar = () => {
     return (
-        <nav className='bg-white col-span-1 lg:col-span-2 row-span-6 p-1 md:p-4 shadow-lg shadow-slate-500 h-fit lg:min-h-screen relative'>
+        <nav className='bg-white col-span-1 lg:col-span-2 row-span-6 p-1 md:p-4 shadow-lg shadow-slate-500 min-h-screen   relative '>
             <header className=" mb-16">
                 <h1 className='text-center font-bold hidden lg:block'>Mohammed Khaled</h1>
                 <h1 className='text-center font-bold lg:hidden text-2xl pt-4'>M</h1>
                 <div className='divider'></div>
             </header>
 
-            <main className="flex flex-col items-center lg:items-start">
+            <main className="flex flex-col items-center lg:items-start fixed left-0 z-20 sm:left-[2%]">
                 {
                     linksData.map((linkData) =>
-                        <Tooltip key={uuid()} content={linkData.name} direction="right">
+                        <Tooltip key={uuid()} content={linkData.name} direction="right" delay={1500}>
                             <NavLink end={linkData.name === "Dashboard"} className={({ isActive }) => isActive ? "active link" : "link"} to={`${linkData.name === "Dashboard" ? "/" : linkData.name.toLocaleLowerCase()}`}>
                                 <span className="flex items-center text-lg">{<linkData.icon />}</span>
                                 <h5 className="hidden lg:block">{linkData.name}</h5>
