2a08afda7641faefd77c87a622169d93eeb08103
Mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-10-08 20:01:44 +0200

    ..

---
+++ src/components/dashboard/LatestNews.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const LatestNews = () => {
+    return (
+        <article className='flex flex-col gap-4 py-4 px-2 justify-start'>
+            <header>
+                <h3 className='font-bold '>Latest News</h3>
+            </header>
+            <main className='grid grid-cols-2 gap-2'>
+
+            </main>
+        </article>
+    )
+}
+
+export default LatestNews
