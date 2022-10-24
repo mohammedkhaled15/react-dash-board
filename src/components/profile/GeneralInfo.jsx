9f3e01d40075542811f14c434ef7acd21bc3625b
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-10-24 12:46:10 +0200

    Profile

---
+++ src/components/profile/GeneralInfo.jsx
@@ -0,0 +1,89 @@
+import Profile from "../../assets/imgs/man.jpg"
+import Divider from "../utilitis/Divider"
+import Star from "../utilitis/Star"
+
+const information = [
+    {
+        name: "General Information",
+        info1T: "Full Name:",
+        info1S: "Mohammed Khaled",
+        info2T: "Gender:",
+        info2S: "Male",
+        info3T: "Country:",
+        info3S: "Egypt",
+        info4: true,
+    },
+    {
+        name: "Personal Information",
+        info1T: "Email:",
+        info1S: "MohammedKhaled1590@gmail.com",
+        info2T: "Phone:",
+        info2S: "0123456789",
+        info3T: "Date Of Birth:",
+        info3S: "01/05/1990",
+        info4: false,
+    },
+    {
+        name: "Job Information",
+        info1T: "Title:",
+        info1S: "Front End Developer",
+        info2T: "Programming Language:",
+        info2S: "Js",
+        info3T: "Years of Exp:",
+        info3S: "15+",
+        info4: true,
+    },
+    {
+        name: "Billing Information",
+        info1T: "Payment Method:",
+        info1S: "Visa",
+        info2T: "Email:",
+        info2S: "email@domain.com",
+        info3T: "Subscription:",
+        info3S: "Monthly",
+        info4: false,
+    },
+
+]
+
+const GeneralInfo = () => {
+    return (
+        <article className='grid grid-cols-4 py-4 px-2 justify-start col-span-12'>
+            <aside className="col-span-1 flex flex-col gap-2 justify-center items-center">
+                <div className="w-[50%] rounded-full overflow-hidden ">
+                    <img src={Profile} alt="profile" className="w-full" />
+                </div>
+                <h3 className="font-bold">Mohammed Khaled</h3>
+                <h6 className="text-text-gray">Level 20</h6>
+                <div className="w-[80%] bg-gray-200 rounded-full h-1">
+                    <div className="bg-blue-600 h-1 rounded-full" style={{ width: "80%" }}></div>
+                </div>
+                <div className="flex items-center ">
+                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" ><Star /></svg>
+                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" ><Star /></svg>
+                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" ><Star /></svg>
+                    <svg className="w-5 h-5 text-yellow-400" fill="currentColor" ><Star /></svg>
+                    <svg className="w-5 h-5 text-text-gray" fill="currentColor" ><Star /></svg>
+                </div>
+                <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">400 Rating</p>
+            </aside>
+            <main className="col-span-3 flex flex-col gap-1 mt-4">
+                {
+                    information.map((info, index) =>
+                        <section key={index} className="">
+                            <h6 className="mb-4 text-text-gray font-bold">{info.name}</h6>
+                            <div className="flex flex-row gap-4 justify-start">
+                                <h6 className="text-xs  font-thin"><span className="text-text-gray font-bold">{info.info1T}</span> {info.info1S}</h6>
+                                <h6 className="text-xs  font-thin"><span className="text-text-gray font-bold">{info.info2T}</span> {info.info2S}</h6>
+                                <h6 className="text-xs  font-thin"><span className="text-text-gray font-bold">{info.info3T}</span> {info.info3S}</h6>
+                            </div>
+                            <Divider index={index} array={information} />
+                        </section>
+                    )
+                }
+            </main>
+        </article>
+    )
+}
+
+export default GeneralInfo
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import GeneralInfo from "./GeneralInfo"
 
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <section className='lg:col-span-10 col-span-11 profile'>
+            <h1 className='title'>Profile</h1>
+            <main className='grid grid-cols-12 gap-2'>
+                <GeneralInfo />
+            </main>
+        </section>
     )
 }
 
