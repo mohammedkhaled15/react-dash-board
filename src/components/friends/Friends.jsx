7f8f2289fc60a0cd8ca9260d49dae362260520e1
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2022-11-28 19:43:31 +0200

    ..

--- src/components/friends/Friends.jsx
+++ src/components/friends/Friends.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import img from "../../assets/imgs/profile.jpg"
 import Divider from "../utilitis/Divider"
 import { BsFillTelephoneFill } from "react-icons/bs"
+import { BsEnvelope } from "react-icons/bs"
+import { BiSmile } from "react-icons/bi"
+import { MdLink } from "react-icons/md"
+import { MdFeed } from "react-icons/md"
 
 const friends = Array(10).fill(
   {
@@ -19,21 +23,31 @@ const Friends = () => {
   return (
     <section className='lg:col-span-10 col-span-11'>
       <h1 className='title'>Friends</h1>
-      <main className='grid grid-cols-10 gap-2'>
+      <main className='grid grid-cols-12 gap-2'>
         {
           friends.map((friend, index) =>
-            <article key={index} className='relative flex flex-col gap-2 col-span-10 md:col-span-5 lg:col-span-2 p-2'>
-              <span><img src={BsFillTelephoneFill} alt="tele" /></span>
-              <span><img src={BsFillTelephoneFill} alt="tele" /></span>
-              <div className="w-[30%] rounded-full  overflow-hidden">
+            <article key={index} className='relative flex flex-col gap-2 col-span-12 md:col-span-4 lg:col-span-3 p-2'>
+              <span className='absolute left-2 bg-[#eee] rounded-full p-1'><BsFillTelephoneFill /></span>
+              <span className='absolute left-10 bg-[#eee] rounded-full p-1'><BsEnvelope /></span>
+              <div className="w-[30%] mt-8 mx-auto rounded-full  overflow-hidden">
                 <img src={friend.img} className="w-full object-center object-cover" alt="bg" />
               </div>
-              <div className="px-2">
+              <div className="px-2 mx-auto">
                 <h2 className="font-bold">{friend.name}</h2>
                 <h6 className='text-text-gray text-2xs text-justify'>{friend.title}</h6>
               </div>
               <Divider />
-
+              <div className='py-2'>
+                <span className='flex flex-row gap-2 items-center font-bold'><BiSmile size={20} />{friend.noOfFriends} Friends</span>
+                <span className='flex flex-row gap-2 items-center font-bold'><MdLink size={20} />{friend.noOfProjects} Projects</span>
+                <span className='flex flex-row gap-2 items-center font-bold'><MdFeed size={20} />{friend.noOfArticles} Articles</span>
+              </div>
+              <Divider />
+              <div className='flex flex-row gap-1 items-center justify-center'>
+                <span className='text-text-gray text-xs'>Joined {friend.dateJoined}</span>
+                <button className='btn btn-xs btn- btn-primary bg-[#0477FF] border-none'>Profile</button>
+                <button className='btn btn-xs btn-error'>Remove</button>
+              </div>
             </article>
           )
         }
